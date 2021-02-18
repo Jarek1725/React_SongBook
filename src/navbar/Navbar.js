b3e53f3de4ea6969224bed81cb1d657020e0f3f5
Jarek1725 <tomaszewskijarek1725@gmail.com>
2021-02-18 01:35:24 +0100

    Test

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './navbar_style.css'
+
+const Navbar = () =>{
+    return  <nav className="navbar">
+                <div className="navbar-container">
+                    <div className="navbar-left">
+                        <h1>FirstApp</h1>
+                    </div>
+                </div>
+            </nav>
+}
+
+export default Navbar;
