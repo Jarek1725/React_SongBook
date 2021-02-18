ab3cc0bc54cc4d49906a7e6f0be172093ac8c895
Jarek1725 <tomaszewskijarek1725@gmail.com>
2021-02-18 10:47:05 +0100

    Navbar is coming

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -6,6 +6,15 @@ const Navbar = () =>{
                     <div className="navbar-left">
                         <h1>FirstApp</h1>
                     </div>
+                    <div className="navbar-center">
+                        <a href="">Add Song</a>
+                        <a href="">Most popular songs</a>
+                        <a href="">Most popular albums</a>
+                        <a href="">Login</a>
+                    </div>
+                    <div className="navbar-right">
+                        Tu ma być search
+                    </div>
                 </div>
             </nav>
 }
